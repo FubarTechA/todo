e7ad806515b30911373397b982b8a94ab78ca54e
FubarTechA <tjfubar11@gmail.com>
2022-05-28 00:14:32 +0100

    new commit

--- script.js
+++ script.js
@@ -10,24 +10,78 @@ const circle = document.querySelectorAll(".circle");
 const todoList = document.querySelectorAll(".todoList");
 const todoInfo = document.querySelectorAll(".todoInfo");
 const functionDiv = document.querySelector(".functions");
-console.log(functionDiv);
+const stateDiv = functionDiv.querySelector(".state");
+const functionPara = functionDiv.querySelectorAll("p");
+// console.log(todoInfo);
 
 class App {
   constructor() {
     this.md = ["sun", "moon"];
-    this.changeMode();
+    this.modeArray = [main, header, input, todoContainer, functionDiv];
+    mode.addEventListener("click", this.changeMode.bind(this));
+    todoList.forEach((list) =>
+      list.addEventListener("click", this.completeTask.bind(this))
+    );
+    this.showActive();
   }
 
   changeMode() {
-    mode.addEventListener("click", function () {
-      header.classList.toggle("light");
-      main.classList.toggle("light");
-      input.classList.toggle("light");
-      todoContainer.classList.toggle("light");
-      functionDiv.classList.toggle("light");
-      circle.forEach((circle) => circle.classList.toggle("light"));
-      todoList.forEach((list) => list.classList.toggle("light"));
-      todoInfo.forEach((list) => list.classList.toggle("light"));
+    if (mode.dataset.mode === "moon") {
+      this.changeModeImg("sun");
+    } else {
+      this.changeModeImg("moon");
+    }
+
+    // toggling the light class on array of elements to change mode
+    this.toggleLightMode();
+  }
+  toggleLightMode() {
+    const forEachArray = [
+      this.modeArray,
+      circle,
+      todoList,
+      todoInfo,
+      functionPara,
+    ];
+    forEachArray.forEach((arr) =>
+      arr.forEach((arrayEl) => arrayEl.classList.toggle("light"))
+    );
+  }
+
+  changeModeImg(string) {
+    let newSrc = `/images/icon-${mode.dataset.mode}.svg`;
+    mode.setAttribute("src", newSrc);
+    mode.dataset.mode = string;
+  }
+
+  completeTask(event) {
+    // marks the clicked option as a completed task
+    this.markComplete(event);
+  }
+
+  markComplete(ev) {
+    let item = ev.target.closest(".todoList");
+    const task = item.querySelector(".todoInfo");
+    const circle = item.querySelector(".circle");
+    const check = circle.querySelector("img");
+    task.classList.add("completed");
+    circle.classList.add("completed");
+    check.classList.add("show");
+  }
+
+  showActive() {
+    stateDiv.addEventListener("click", function (e) {
+      let para = e.target;
+      let siblings = para.closest(".state").querySelectorAll("p");
+      console.log(siblings);
+      siblings.forEach((sib) => {
+        if (sib === para) return;
+        sib.classList.remove("active");
+      });
+      para.classList.add("active");
+      if (para.textContent === "All") {
+        console.log(todoInfo);
+      }
     });
   }
 }
